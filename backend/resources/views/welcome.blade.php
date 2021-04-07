e8079a05f6cc713fff17d5912d72f80debac6a79
nekulala <y.kurabayashi1031@gmail.com>
2021-04-07 22:44:32 +0900

    新規登録及びログイン実装

--- backend/resources/views/welcome.blade.php
+++ backend/resources/views/welcome.blade.php
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    ツイッタークローン
                 </div>
 
                 <div class="links">
