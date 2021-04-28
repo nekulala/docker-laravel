cba995709d6b760a3b6737c2f45ab9595fe1189d
nekulala <y.kurabayashi1031@gmail.com>
2021-04-28 14:54:23 +0900

    不要use削除

--- backend/app/Http/Controllers/HomeController.php
+++ backend/app/Http/Controllers/HomeController.php
@@ -5,7 +5,6 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use App\Models\Tweet;
-use App\Models\Favorite;
 
 class HomeController extends Controller
 {
